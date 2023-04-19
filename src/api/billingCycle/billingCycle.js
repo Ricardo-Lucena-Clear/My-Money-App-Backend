67493b77bd780e33397acadbe17233289ebfaf5d
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-19 08:53:11 -0300

    Testing the API (Postman)

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -6,9 +6,10 @@ const creditSchema = new mongoose.Schema({
     value: {type: Number, min:0, required: true}
 })
 const debtSchema = new mongoose.Schema({
-    name: {type:String, required: true},
-    value: {type: Number, min:0, required: true},
-    status: {type:String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE','AGENDADO']}
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
+    status: { type: String, required: false, uppercase: true,
+        enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
 
 const billingCycleSchema = new mongoose.Schema({
