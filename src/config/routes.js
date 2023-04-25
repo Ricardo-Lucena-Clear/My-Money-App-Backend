6b9cb50393183d142fc3ea2e3953aace243f3646
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-25 11:10:38 -0300

    Fix Project

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,23 +1,10 @@
-const express = require('express')
-const auth = require('./auth')
+const express = require ('express')
 
-module.exports = function (server) {
-    /*
-     * Rotas protegidas por Token JWT
-     */
-    const protectedApi = express.Router()
-    server.use('/api', protectedApi)
-    protectedApi.use(auth)
-    const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(protectedApi, '/billingCycles')
-    /*
-     * Rotas abertas
-     */
-    const openApi = express.Router()
-    server.use('/oapi', openApi)
-    
-    const AuthService = require('../api/user/AuthService')
-    openApi.post('/login', AuthService.login)
-    openApi.post('/signup', AuthService.signup)
-    openApi.post('/validateToken', AuthService.validateToken)
+module.exports= function(server){
+    const router = express.Router()
+    server.use('/api', router)
+
+
+    const BillingCycle = require ('../api/billingCycle/billingCycleService')
+    BillingCycle.register(router, '/billingCycles')
 }
